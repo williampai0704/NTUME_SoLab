09326940c7cc877ecf2c9457861960397e3dc926
williampai <williampai0704@gmail.com>
2022-10-16 12:17:25 +0800

    Add Road

--- simulation.py
+++ simulation.py
@@ -16,11 +16,15 @@ class Car:
   gamma = 0.6
   lmda = 1
   
-  def __init__(self, x,y,vel,steerAngle,lane):
-    self.x = x
+  def __init__(self,lane,y,vel,steerAngle):
+    if lane == 'L':
+      self.x = windowWidth/2 - meter2pixel(lineWidth/2+roadWidth/2)
+    if lane == 'R':
+      self.x = windowWidth/2 + meter2pixel(lineWidth/2+roadWidth/2)
+      
     self.y = y
-    self.x_c = x+carWidth_p/2 #center x
-    self.y_c = y+carLength_p/2 #center y
+    self.x_tl = self.x-meter2pixel(carWidth)/2  #top left x
+    self.y_tl = self.y-meter2pixel(carLength)/2 #top left y
     self.vel = vel
     self.steerAngle = steerAngle
     self.lane = lane
@@ -29,12 +33,12 @@ class Car:
   def drive(self):
     self.x += self.vel*math.sin(self.steerAngle)
     self.y -= self.vel*math.cos(self.steerAngle)
-    self.x_c += self.vel*math.sin(self.steerAngle)
-    self.y_c -= self.vel*math.cos(self.steerAngle)
+    self.x_tl += self.vel*math.sin(self.steerAngle)
+    self.y_tl -= self.vel*math.cos(self.steerAngle)
       
   def cal_prob(self,TCar):
-    self.D = pixel2meter(math.sqrt((self.x_c- TCar.x_c)**2+(self.y_c - TCar.y_c)**2))
-    self.alpha1 = math.atan2(-(TCar.x_c-self.x_c),-(TCar.y_c-self.y_c)) #radius 
+    self.D = pixel2meter(math.sqrt((self.x- TCar.x)**2+(self.y - TCar.y)**2))
+    self.alpha1 = math.atan2(-(TCar.x-self.x),-(TCar.y-self.y)) #radius 
 
     if (self.alpha1 <= math.pi/2) & (self.alpha1 >= -math.pi/2):
       self.R = math.sqrt((self.gamma*self.l*math.cos(self.alpha1))**2+(1/2*self.w*math.sin(self.alpha1))**2)
@@ -86,28 +90,51 @@ def meter2pixel(m):
 def pixel2meter(p):
   return p/30
 
+def drawRoad():
+  tW = meter2pixel(totalWidth)
+  rW = meter2pixel(roadWidth)
+  lW = meter2pixel(lineWidth)
+  lL = meter2pixel(lineLength)
+  lS = meter2pixel(lineSpace)
+  road = pygame.Rect(0,0,tW,windowLength)
+  line = pygame.Rect(0,0,lW,windowLength)
+  road.center = (windowWidth/2,windowLength/2)
+  line.center = (windowWidth/2,windowLength/2)
+  screen.fill([0,0,0])
+  pygame.draw.rect(screen,'gray70',road)
+  pygame.draw.rect(screen,'yellow',line)
+  
+#Length 
+windowWidth = 600 #pixel
+windowLength = 800
+carWidth = 2.4 #m
+carLength = 4.8
+totalWidth = 8 #m
+roadWidth = 3.9
+lineWidth = 0.2
+lineLength = 4
+lineSpace = 6
+
 pygame.init()
-screen = pygame.display.set_mode((800,1000))
+screen = pygame.display.set_mode((windowWidth,windowLength))
 pygame.display.set_caption("SoLab")
-carWidth_p = 24*3
-carLength_p = 48*3
 whiteCar = pygame.image.load('pics/white_car.png')
-whiteCar = pygame.transform.scale(whiteCar, (carLength_p, carWidth_p))
+whiteCar = pygame.transform.scale(whiteCar, (meter2pixel(carLength), meter2pixel(carWidth)))
 whiteCar = pygame.transform.rotate(whiteCar, 90)
 redCar = pygame.image.load('pics/red_car.png')
-redCar = pygame.transform.scale(redCar,(carLength_p, carWidth_p))
+redCar = pygame.transform.scale(redCar,(meter2pixel(carLength), meter2pixel(carWidth)))
 redCar = pygame.transform.rotate(redCar, 90)
 clock = pygame.time.Clock()
 
-egoCar = Car(400,300,0,0,'R')
-LFCar = Car(328,800,5,0,'L')
+egoCar = Car('R',400,0,0)
+LFCar = Car('L',800,5,0)
 
 def refreshScreen(egoCar,LFCar):
-  screen.fill([0,0,0])
+  drawRoad()
   egoCar.drive()
   LFCar.drive()
-  screen.blit(whiteCar,(egoCar.x,egoCar.y))
-  screen.blit(redCar,(LFCar.x,LFCar.y))
+  screen.blit(whiteCar,(egoCar.x_tl,egoCar.y_tl))
+  screen.blit(redCar,(LFCar.x_tl,LFCar.y_tl))
   egoCar.cal_prob(LFCar)
   pygame.display.update()
 
