8d5e3ebf68f4d8f73260963c15b344a8f91ffba6
williampai <williampai0704@gmail.com>
2022-11-23 23:47:37 +0800

    add velocityProfile

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ from math import exp
 import matplotlib.pyplot as plt
 from bezier import Bezier
 from bezier import RPoint
+from velocityProfile import velocityProfile
 
 class Car:
   w     = 2.4 #car width (m)
@@ -14,7 +15,7 @@ class Car:
   mode  = ['stand_by','lane_change']
   i = 0
   
-  def __init__(self,lane,y,vel,acc,steerAngle):
+  def __init__(self,lane,y,vel,steerAngle,isFront):
     if lane == 'L':
       self.x = windowWidth/2 - meter2pixel(lineWidth/2+roadWidth/2) #pixel
     if lane == 'R':
@@ -24,10 +25,12 @@ class Car:
     self.y_ref = meter2pixel(y)
     self.x_tl = self.x-meter2pixel(carWidth)/2  # top left x
     self.y_tl = self.y-meter2pixel(carLength)/2 # top left y
-    self.vel = meter2pixel(vel*1000/(10*3600)) # from km/hr --> pixel/fps
+    self.vel = km_hr2pixel_fps(vel) # from km/hr --> pixel/fps
+    self.vel2 = vel
+    self.vel_ideal = vel
     self.steerAngle = steerAngle/180*math.pi
     self.lane = lane   
-    self.acc = meter2pixel(acc)/10 #acceleration
+    # self.acc = meter2pixel(acc)/10 #acceleration
     self.probList   = [[] for i in range(4)]
     self.timeList   = [[] for i in range(4)]
     self.timeList2  = [[] for i in range(4)]
@@ -40,10 +43,21 @@ class Car:
     self.xList      = [[] for i in range(4)]
     self.yList      = [[] for i in range(4)]
     self.mode = 'stand_by'
+    self.isFront = isFront
 
   def drive(self,egoCar):
     global lane_changing
-    self.vel += self.acc
+    global finish_lane_change
+    global start_lane_change
+    # self.vel += self.acc
+    if not self.isFront:
+      if not start_lane_change:
+        self.vel = km_hr2pixel_fps(velocityProfile(self.probList[1][-1],self.vel_ideal))
+      if lane_changing:
+        self.vel = km_hr2pixel_fps(velocityProfile(self.probList[2][-1],self.vel_ideal))
+      if finish_lane_change:
+        self.vel = km_hr2pixel_fps(velocityProfile(self.probList[0][-1],self.vel_ideal))
+        
     if self.mode == 'stand_by':
       self.steerAngle = 0
       self.x += self.vel*math.sin(self.steerAngle) # Real x(pixel)
@@ -54,8 +68,8 @@ class Car:
         self.i = len(self.b_x)-1
         self.mode = 'stand_by'
         print("back to stand by")
-        
-        lane_changing = 0
+        finish_lane_change = True
+        lane_changing = False
       self.x = self.b_x[self.i]
       self.y = self.b_y[self.i]
       self.steerAngle = self.b_alpha1[self.i]
@@ -64,7 +78,7 @@ class Car:
     self.x_tl = self.x-meter2pixel(carWidth)/2  #top left x
     self.y_tl = self.y_ref-meter2pixel(carLength)/2 #top left y
     for i in range(4):
-      self.vList[i].append(self.vel)
+      self.vList[i].append(pixel_fps2km_hr(self.vel))
       self.xList[i].append(pixel2meter(self.x))
       self.yList[i].append(pixel2meter(self.y))
       self.timeList2[i].append(pygame.time.get_ticks()/1000)
@@ -91,6 +105,8 @@ def cal_prob(eCar,TCar,i):
   else:
     eCar.r = math.sqrt(((1-eCar.gamma)*eCar.l*math.cos(eCar.alpha2_h))**2+(1/2*eCar.w*math.sin(eCar.alpha2_h))**2) 
   
+  eCar.lmda2 = 1 - ((eCar.vel-80)/80)
+  print(eCar.lmda2)
   #Calculate probability
   if eCar.D - eCar.R - eCar.r > 0:
     eCar.prob = eCar.lmda*exp(-eCar.lmda2*(eCar.D-eCar.R-eCar.r))
@@ -105,7 +121,7 @@ def cal_prob(eCar,TCar,i):
   eCar.RList[i].append(eCar.R)
   eCar.rList[i].append(eCar.r)
   
-def plot_prob(TCar,i):
+def plot1(TCar,i):
   plt.subplot(121)
   plt.plot(TCar.timeList[i],TCar.probList[i])
   # plt.subplot(332)
@@ -127,12 +143,21 @@ def plot_prob(TCar,i):
   ax = plt.gca()
   ax.set_aspect('equal', adjustable='box')
   plt.show()
+
+def plot2(TCar,i):
+  plt.plot(TCar.timeList2[i],TCar.vList[i])
+  # plt.subplot(338)
+  plt.show()
   
 
 def meter2pixel(m):
   return m*20
 def pixel2meter(p):
   return p/20
+def km_hr2pixel_fps(v):
+  return meter2pixel(v*1000/(10*3600))
+def pixel_fps2km_hr(p):
+  return pixel2meter(p/1000*3600*10)
 
 def drawRoad():
   tW = meter2pixel(totalWidth)
@@ -170,19 +195,28 @@ redCar = pygame.transform.scale(redCar,(meter2pixel(carLength), meter2pixel(carW
 redCar = pygame.transform.rotate(redCar, 90)
 clock = pygame.time.Clock()
 
-egoCar = Car('R',20,80,0,0)
-LFCar  = Car('L',5,81,0,0)
-FCar   = Car('R',10,75,0,0)
-LRCar  = Car('L',35,83,0,0)
-RCar   = Car('R',35,80,0,0)
+egoCar = Car('R',20,80,0,False)
+LFCar  = Car('L',5,80,0,True)
+FCar   = Car('R',10,77,0,True)
+LRCar  = Car('L',35,85,0,False)
+RCar   = Car('R',35,90,0,False)
 start_lane_change = False
 lane_changing = False
+finish_lane_change = False
 
 
 def refreshScreen():
   drawRoad()
-  cal_prob(egoCar,LFCar,0)
-  cal_prob(egoCar,FCar,1)
+  cal_prob(egoCar,LFCar,0) # 0 = track after lane change
+  cal_prob(RCar,FCar,0)
+  cal_prob(LRCar,egoCar,0)
+  cal_prob(egoCar,FCar,1) # 1 = track before lane change 
+  cal_prob(LRCar,LFCar,1)
+  cal_prob(RCar,egoCar,1)
+  cal_prob(egoCar,LFCar,2) # 2 = when lane changing
+  cal_prob(LRCar,egoCar,2) 
+  cal_prob(RCar,egoCar,2)
+  
   car_move()
   screen.blit(whiteCar,(egoCar.x_tl,egoCar.y_tl))
   screen.blit(redCar,(LFCar.x_tl,LFCar.y_tl))
@@ -210,7 +244,7 @@ def car_move():
   FCar.drive(egoCar)
   LRCar.drive(egoCar)
   RCar.drive(egoCar)
-  print(str(egoCar.steerAngle) + "\n")
+  # print(str(egoCar.steerAngle) + "\n")
   
 
 def main():
@@ -224,8 +258,8 @@ def main():
     refreshScreen()
       
   # plot_prob(egoCar,0)
-  plot_prob(egoCar,1)
-  
+  plot1(egoCar,1)
+  plot2(LRCar,1)
   pygame.quit()
 
 if __name__ == '__main__':
---
+++ velocityProfile.py
@@ -0,0 +1,16 @@
+import math
+from math import exp
+import numpy as np
+import matplotlib.pyplot as plt
+
+def velocityProfile(currentP,idealV):
+    return -idealV/2*((np.tanh(5*(currentP-0.6)))+1)+idealV
+
+pList = [i/100 for i in range(101)]
+v = []
+for i in range(101):
+    v.append(velocityProfile(i/100,60))
+
+# plt.plot(pList ,v)
+# plt.grid(True)
+# plt.show()
